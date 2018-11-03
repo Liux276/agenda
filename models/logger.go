6b456de64652d6b19a482eb4af9371cef9f6d13d
LX <liux276@163.com>
2018-11-03 19:04:52 +0800

    更新models

--- models/logger.go
+++ models/logger.go
@@ -1,15 +1,39 @@
 package models
 
 import (
-	"os"
 	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
 )
 
 var Logger *log.Logger
+var ExecPath string
 
-func init(){
+func init() {
+	for _, arg := range os.Args {
+		if strings.Split(arg, "=")[0] == "go_test" {
+			ExecPath = strings.Replace(strings.Split(arg, "=")[1], "\\", "/", -1)
+			if ExecPath[len(ExecPath)-1] != '/' {
+				ExecPath += "/"
+			}
+			return
+		}
+	}
+	file, _ := exec.LookPath(os.Args[0])
+	path, _ := filepath.Abs(file)
+	index := strings.LastIndex(path, string(os.PathSeparator))
+
+	// the current path will be $GOPATH/bin
+	// so I here truncate the bin and add src to get $GOPATH/src
+	ExecPath = path[:index-3] + "src/"
+	ExecPath = strings.Replace(ExecPath, "\\", "/", -1)
+}
+
+func init() {
 	var logHandler *os.File
-	logHandler, err := os.OpenFile("github.com/sysu-615/agenda/log/logFile.txt", os.O_APPEND, 0666)
+	logHandler, err := os.OpenFile(ExecPath+"github.com/sysu-615/agenda/log/logFile.txt", os.O_APPEND, 0666)
 	if err != nil {
 		panic(err)
 	}
